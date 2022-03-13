f8b8452527dc358749d06bf6832b3004c0e4ae32
Annetkhavere <anneywan5@gmail.com>
2022-03-13 15:18:28 +0300

    apply toggle between the description of each icon

--- js/script.js
+++ js/script.js
@@ -2,5 +2,11 @@ $(document).ready(function() {
   $("#designimg").click(function() {
     $(".p-design").toggle();
   });
+  $("#devtimg").click(function() {
+    $(".p-devt").toggle();
+  });
+  $("#productimg").click(function() {
+    $(".p-product").toggle();
+  });
 
 });
