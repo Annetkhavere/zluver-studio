898a2b3ceee6245a8a3e6fe305162229f082c846
Annetkhavere <anneywan5@gmail.com>
2022-03-13 19:00:53 +0300

    add a hover effect on the images under portfolio

--- js/script.js
+++ js/script.js
@@ -24,4 +24,70 @@ $(document).ready(function() {
     $("#productimg").show();
   });
 
-});
+
+  $("#work4").hover(
+    function() {
+      $(".overlay4").css("display", "block");
+    },
+    function() {
+      $(".overlay4").css("display", "none");
+    });
+
+  $("#work3").hover(
+    function() {
+       $(".overlay3").css("display", "block");
+      },
+    function() {
+       $(".overlay3").css("display", "none");
+      });
+
+  $("#work2").hover(
+    function() {
+      $(".overlay2").css("display", "block");
+        },
+    function() {
+      $(".overlay2").css("display", "none");
+     });
+
+  $("#work1").hover(
+    function() {
+      $(".overlay1").css("display", "block");
+       },
+    function() {
+      $(".overlay1").css("display", "none");
+       });
+  
+  $("#work5").hover(
+    function() {
+      $(".overlay5").css("display", "block");
+         },
+    function() {
+      $(".overlay5").css("display", "none");
+        });
+  $("#work6").hover(
+    function() {
+      $(".overlay6").css("display", "block");
+          },
+    function() {
+      $(".overlay6").css("display", "none");
+       });
+
+  $("#work7").hover(
+    function() {
+      $(".overlay7").css("display", "block");
+        },
+    function() {
+      $(".overlay7").css("display", "none");
+       });
+  
+  $("#work8").hover(
+    function() {
+      $(".overlay8").css("display", "block");
+       },
+    function() {
+      $(".overlay8").css("display", "none");
+        });
+
+ });
+
+
