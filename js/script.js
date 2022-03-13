a8914f2594ea4ce7707de13ebdc2172faf965385
Annetkhavere <anneywan5@gmail.com>
2022-03-13 22:15:50 +0300

    change #contactus top to 2300px

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+// what we do section
   $("#designimg").click(function() {
     $("#designimg").hide();
     $(".p-design").show();
@@ -24,7 +25,7 @@ $(document).ready(function() {
     $("#productimg").show();
   });
 
-
+// portfolio section
   $("#work4").hover(
     function() {
       $(".overlay4").css("display", "block");
@@ -87,6 +88,30 @@ $(document).ready(function() {
     function() {
       $(".overlay8").css("display", "none");
         });
+
+// form section
+let form = document.getElementById('form');
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault(); //cancel the default behavior for the event.
+  popUp();
+});
+ 
+function popUp () {
+  let nameInput = document.getElementById('name').value;
+  let emailInput = document.getElementById('email').value;
+  let messageInput = document.getElementById('message').value;
+
+  if(nameInput == "") {
+    alert("Please insert your name to proceed");
+  } else if(emailInput == "") {
+    alert("Please insert your email to proceed");
+  } else if(messageInput == "") {
+    alert("Please insert your message to submit");
+  } else {
+  alert("We have received your message "+ nameInput  + ". Thank you for reaching out to us");
+  }
+}
 
  });
 
